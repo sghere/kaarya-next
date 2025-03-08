7bd998a54f9d0dcdd32bee961d16cd59a66a883b
sghere <gshubham8@gmail.com>
2025-03-08 23:20:40 +0530

    feat: page router

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
+
 const AppHeader = () => {
-  return <header className="header">AppHeader</header>;
+  const pathname = usePathname();
+  const lastSegment = pathname.split("/").filter(Boolean).pop();
+  const formattedTitle = lastSegment
+    ? lastSegment.charAt(0).toUpperCase() + lastSegment.slice(1)
+    : "Home";
+
+  const router = useRouter();
+  return (
+    <header className="header flex items-center justify-between px-6 h-16 text-white">
+      <GoChevronLeft
+        className="size-6 cursor-pointer"
+        onClick={() => router.back()}
+      />
+      <h1 className="text-lg font-semibold text-center">{formattedTitle}</h1>
+      {/* <GoChevronRight className="size-6" /> */}
+      <label htmlFor=""></label>
+    </header>
+  );
 };
 
 export default AppHeader;
